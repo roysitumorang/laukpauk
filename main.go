d19621843dc126c5bb431ead3dcfb958c1ed0347
Roy Situmorang <roy.situmorang@gmail.com>
2023-10-24 08:35:45 +0700

    Add feature migration.

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
-	"log"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/roysitumorang/laukpauk/config"
+	"github.com/roysitumorang/laukpauk/helper"
 	"github.com/roysitumorang/laukpauk/router"
 	"github.com/roysitumorang/laukpauk/services/messagingconsumer"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
+	ctxt := "Main"
+	ctx := context.Background()
 	cmdVersion := &cobra.Command{
 		Use:   "version",
 		Short: "print version",
@@ -25,7 +31,7 @@ func main() {
 		Short: "run app",
 		Run: func(_ *cobra.Command, _ []string) {
 			if err := godotenv.Load(".env"); err != nil {
-				log.Fatalln("can't load .env file")
+				helper.Capture(ctx, zap.FatalLevel, err, ctxt, "ErrLoad")
 			}
 			var g errgroup.Group
 			service := router.MakeHandler()
@@ -38,7 +44,40 @@ func main() {
 				return nil
 			})
 			if err := g.Wait(); err != nil {
-				log.Fatal(err)
+				helper.Capture(ctx, zap.FatalLevel, err, ctxt, "ErrWait")
+			}
+		},
+	}
+	cmdMigration := &cobra.Command{
+		Use:   "migration",
+		Short: "new/run migration",
+		Args: func(_ *cobra.Command, args []string) (err error) {
+			if len(args) == 0 {
+				err = errors.New("requires at least 1 arg (new|run")
+				return
+			}
+			if args[0] != "new" && args[0] != "run" {
+				err = fmt.Errorf("invalid first flag specified: %s", args[0])
+			}
+			return
+		},
+		Run: func(_ *cobra.Command, args []string) {
+			if err := godotenv.Load(".env"); err != nil {
+				helper.Capture(ctx, zap.FatalLevel, err, ctxt, "ErrLoad")
+			}
+			service := router.MakeHandler()
+			now := time.Now()
+			switch args[0] {
+			case "new":
+				if err := service.Migration.CreateMigrationFile(ctx); err != nil {
+					helper.Capture(ctx, zap.FatalLevel, err, ctxt, "ErrCreateMigrationFile")
+				}
+				duration := time.Since(now)
+				helper.Log(ctx, zap.InfoLevel, fmt.Sprintf("creating migration successfully in %s", duration.String()), ctxt, "")
+			case "run":
+				service.Migration.Migrate(ctx)
+				duration := time.Since(now)
+				helper.Log(ctx, zap.InfoLevel, fmt.Sprintf("running migration successfully in %s", duration.String()), ctxt, "")
 			}
 		},
 	}
@@ -46,6 +85,7 @@ func main() {
 	rootCmd.AddCommand(
 		cmdVersion,
 		cmdRun,
+		cmdMigration,
 	)
 	rootCmd.SuggestionsMinimumDistance = 1
 	_ = rootCmd.Execute()
--- router/handler.go
+++ router/handler.go
@@ -1,16 +1,22 @@
 package router
 
 import (
+	"context"
+
 	"github.com/roysitumorang/laukpauk/config"
+	"github.com/roysitumorang/laukpauk/helper"
+	"github.com/roysitumorang/laukpauk/migration"
 	authUseCase "github.com/roysitumorang/laukpauk/modules/auth/usecase"
 	regionQuery "github.com/roysitumorang/laukpauk/modules/region/query"
 	regionUseCase "github.com/roysitumorang/laukpauk/modules/region/usecase"
 	userQuery "github.com/roysitumorang/laukpauk/modules/user/query"
 	userUseCase "github.com/roysitumorang/laukpauk/modules/user/usecase"
+	"go.uber.org/zap"
 )
 
 type (
 	Service struct {
+		Migration     *migration.Migration
 		AuthUseCase   authUseCase.AuthUseCase
 		RegionUseCase regionUseCase.RegionUseCase
 		UserUseCase   userUseCase.UserUseCase
@@ -18,14 +24,23 @@ type (
 )
 
 func MakeHandler() *Service {
+	ctxt := "Router-MakeHandler"
 	dbRead := config.GetDbReadOnly()
 	dbWrite := config.GetDbWriteOnly()
+	ctx := context.Background()
+	tx, err := dbWrite.Begin(ctx)
+	if err != nil {
+		helper.Capture(ctx, zap.FatalLevel, err, ctxt, "ErrBegin")
+		return nil
+	}
+	migration := migration.NewMigration(tx)
 	regionQuery := regionQuery.NewRegionQuery(dbRead, dbWrite)
 	userQuery := userQuery.NewUserQuery(dbRead, dbWrite)
 	authUseCase := authUseCase.NewAuthUseCase(userQuery)
 	regionUseCase := regionUseCase.NewRegionUseCase(regionQuery)
 	userUseCase := userUseCase.NewUserUseCase(userQuery)
 	return &Service{
+		Migration:     migration,
 		AuthUseCase:   authUseCase,
 		RegionUseCase: regionUseCase,
 		UserUseCase:   userUseCase,
